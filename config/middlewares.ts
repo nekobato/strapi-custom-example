adfccd90bb31e59b8cb74235e8a5253db43397d4
nekobato <nekobato@gmail.com>
2025-07-10 12:50:32 +0900

    fix: middleware settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,14 +1,6 @@
 export default ({ env }) => [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
   {
     name: "strapi::security",
     config: {
@@ -38,5 +30,12 @@ export default ({ env }) => [
         }
       }
     }
-  }
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public"
 ];
